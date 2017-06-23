45e6eb97b9aa1be86287f5d75ab4d1b06797c60b
Misha <misha.ostanin1@yandex.ru>
2017-06-23 22:48:53 +0300

    New comit

--- index.php
+++ index.php
@@ -5,10 +5,6 @@
 	<title>Document</title>
 </head>
 <body>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae earum quod aliquid nulla ab obcaecati aut quae, illum voluptates maxime. Mollitia eius voluptas laboriosam, odio quod quisquam aliquid quas obcaecati cumque! Blanditiis at, quam exercitationem ad, quo harum tempore dignissimos!</p>
-	<p>Ullam nesciunt nostrum quaerat consectetur error est numquam earum at officia in sunt, fugit labore sapiente doloremque mollitia eveniet aut cum doloribus optio rem! Magnam nisi tempora voluptatibus at, deserunt porro quis blanditiis molestias possimus perspiciatis, eveniet ad harum dolor.</p>
-	<p>Excepturi dicta, perspiciatis temporibus voluptas nam minus! Quas repellendus culpa ipsa, soluta eveniet. Possimus mollitia sequi odio temporibus officia dicta id, animi magnam praesentium tempora dolores accusantium harum sint sapiente minus voluptas beatae illo. Porro dignissimos excepturi perferendis consequatur voluptates.</p>
-	<p>Temporibus, veritatis, nobis. Error sed, odio, illum excepturi ex obcaecati rerum omnis laudantium repudiandae labore molestiae, harum cum ut, ea doloremque. Dolore veritatis inventore perferendis, ratione adipisci beatae illo et vero iste rem possimus, ipsam dicta fugiat voluptas commodi assumenda!</p>
-	<p>Quia rem earum dolorum sunt laboriosam harum perspiciatis voluptatibus reprehenderit repudiandae, iure distinctio, inventore ea amet soluta maxime modi. Aspernatur quidem inventore dignissimos est soluta libero suscipit harum veniam explicabo, id iusto necessitatibus dolorem voluptatem maiores sunt ullam, cupiditate animi.</p>
+	
 </body>
 </html>
